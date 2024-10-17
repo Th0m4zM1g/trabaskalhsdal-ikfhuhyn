b58bd4d8ba06ef4f45a904c28c3206b237766c80
Th0m4z <137521233+Th0m4zM1g@users.noreply.github.com>
2024-10-17 09:54:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Piada 1',
+    'Como se faz pra engravidar uma freira?',
+    'Veste ela de coroinha'
+)
+
+criaCartao(
+    'Piada 2',
+    'O que tem 2 braços, 2 pernas e 37 furos?',
+    'A Mariele Franco'
+)
+
+criaCartao(
+    'Piada 3',
+    'O que o garoto sem mãos ganhou de presente no seu aniverário?',
+    'Não sei, ele ainda não conseguiu abrir'
+)
+
+criaCartao(
+    'Inglês',
+    'There is only one thing that beats a beautiful girl who can sing',
+    'Chris Brown'
+)
